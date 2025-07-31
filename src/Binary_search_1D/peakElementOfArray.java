60747a49c64a4286901542b6a159271acec1f703
FarmanAhsan01 <farmanahsan77@gmail.com>
2025-07-31 12:15:10 +0530

    find peak element in an array it can have single and multiple peak

---
+++ src/Binary_search_1D/peakElementOfArray.java
@@ -0,0 +1,35 @@
+package Binary_search_1D;
+
+public class peakElementOfArray {
+    public static int findPeakElement(int nums[]){
+        int n=nums.length;
+        if (n==1) return 0;
+        if (nums[0]>nums[1]) return 0;
+        if (nums[n-1]>nums[n-2]) return n-1;
+        int low=1;int high=n-2;
+        while (low<=high){
+            int mid=(low+high)/2;
+            if (nums[mid]>nums[mid+1]&&nums[mid-1]<nums[mid]){
+                return mid;
+            }
+            else if (nums[mid]>nums[mid-1]){
+               low=mid+1;
+            }
+//            for one peak..............................................
+
+            else if (nums[mid]<nums[mid-1]){
+                high=mid-1;
+            }
+            //for.many peaks..................................................
+            else {
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int nums[]={1,2,1,3,5,6,4};
+        System.out.println(findPeakElement(nums));
+    }
+}
