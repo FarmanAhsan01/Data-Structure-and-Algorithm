3b531f17635b25367aee0153d1ffedacaf7dad60
FarmanAhsan01 <farmanahsan77@gmail.com>
2025-10-08 16:10:26 +0530

    print all subsequence without duplicate

--- src/Recursion_ll/printingSubsequenceWhoseSumisK.java
+++ src/Recursion_ll/printingSubsequenceWhoseSumisK.java
@@ -1,6 +1,7 @@
 package Recursion_ll;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class printingSubsequenceWhoseSumisK {
@@ -51,24 +52,50 @@ public class printingSubsequenceWhoseSumisK {
 //        return false;
 //    }
 
-    public static int printS(int ind,int s, int sum, int arr[], int n){
-        if (ind==n){
-            if (s==sum){
-                return 1;
-            }
-            return 0;
-        }
-        s+=arr[ind];
-        int l=printS(ind+1,s,sum,arr,n);
-        s-=arr[ind];
-        int r=printS(ind+1,s,sum,arr,n);
-        return l+r;
+//    public static int printS(int ind,int s, int sum, int arr[], int n){
+//        if (ind==n){
+//            if (s==sum){
+//                return 1;
+//            }
+//            return 0;
+//        }
+//        s+=arr[ind];
+//        int l=printS(ind+1,s,sum,arr,n);
+//        s-=arr[ind];
+//        int r=printS(ind+1,s,sum,arr,n);
+//        return l+r;
+//    }
+
+    public static List<List<Integer>>combinationSum2(int candidates[],int target){
+        List<List<Integer>>ans=new ArrayList<>();
+        Arrays.sort(candidates);
+        findCombinations(0,candidates,target,ans,new ArrayList<>());
+        return ans;
     }
+    public static void findCombinations(int ind,int arr[],int target,List<List<Integer>>ans,List<Integer>ds){
+        if (target==0){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=ind;i<arr.length;i++){
+            if (i>ind && arr[i]==arr[i-1]){
+                continue;
+            }
+            if (arr[i]>target){
+                break;
+            }
+            ds.add(arr[i]);
+            findCombinations(i+1,arr,target-arr[i],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+
     public static void main(String[] args) {
         int arr[]={1,2,1};
         int n=3;
         int sum=2;
-        List<Integer>ds=new ArrayList<>();
-        System.out.println(printS(0,0,sum,arr,n));
+        List<List<Integer>> ans = combinationSum2(arr, sum);
+        System.out.println(ans);
+
     }
 }
