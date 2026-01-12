25e8e40e68f3825bd57d678b0323df688e7a7289
FarmanAhsan01 <farmanahsan77@gmail.com>
2026-01-12 12:40:09 +0530

    largest Rectangle Histogram Problem using optimal approach

--- src/Stack_And_Queues/largestRectangleHistogram.java
+++ src/Stack_And_Queues/largestRectangleHistogram.java
@@ -6,44 +6,67 @@ import java.util.List;
 import java.util.Stack;
 
 public class largestRectangleHistogram {
-    public static List<Integer>findNSE(int arr[]){
-        int n=arr.length;
-        List<Integer>nse=new ArrayList<>();
-        Stack<Integer>st=new Stack<>();
-        for (int i = 0; i < n; i++) {
-            nse.add(n);
-        }
-        for (int i=0;i<n;i++){
-            while(!st.isEmpty() && arr[st.peek()]>arr[i]){
-                nse.set(st.pop(), i);
-            }
-            st.push(i);
-        }
-        return nse;
-    }
-    public static List<Integer> findPSE(int arr[]) {
-        int n = arr.length;
-        List<Integer> pse = new ArrayList<>(Collections.nCopies(n, -1));
-        Stack<Integer> st = new Stack<>();
+//    public static List<Integer>findNSE(int arr[]){
+//        int n=arr.length;
+//        List<Integer>nse=new ArrayList<>();
+//        Stack<Integer>st=new Stack<>();
+//        for (int i = 0; i < n; i++) {
+//            nse.add(n);
+//        }
+//        for (int i=0;i<n;i++){
+//            while(!st.isEmpty() && arr[st.peek()]>arr[i]){
+//                nse.set(st.pop(), i);
+//            }
+//            st.push(i);
+//        }
+//        return nse;
+//    }
+//    public static List<Integer> findPSE(int arr[]) {
+//        int n = arr.length;
+//        List<Integer> pse = new ArrayList<>(Collections.nCopies(n, -1));
+//        Stack<Integer> st = new Stack<>();
+//
+//        for (int i = 0; i < n; i++) {
+//            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+//                st.pop();
+//            }
+//            pse.set(i, st.isEmpty() ? -1 : st.peek());
+//            st.push(i);
+//        }
+//        return pse;
+//    }
+//    public static int largestRectangle(int arr[]){
+//        int n=arr.length;
+//        List<Integer>nse=findNSE(arr);
+//        List<Integer>pse=findPSE(arr);
+//        int maxi=0;
+//        for (int i=0;i<n;i++){
+//            maxi=Math.max(maxi,arr[i]*(nse.get(i)-pse.get(i)-1));
+//        }
+//        return maxi;
+//    }
 
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-                st.pop();
-            }
-            pse.set(i, st.isEmpty() ? -1 : st.peek());
-            st.push(i);
-        }
-        return pse;
-    }
+    //optimal approach.........................................
     public static int largestRectangle(int arr[]){
+        Stack<Integer>stack=new Stack<>();
+        int maxArea=0;
         int n=arr.length;
-        List<Integer>nse=findNSE(arr);
-        List<Integer>pse=findPSE(arr);
-        int maxi=0;
-        for (int i=0;i<n;i++){
-            maxi=Math.max(maxi,arr[i]*(nse.get(i)-pse.get(i)-1));
+
+        for (int i=0;i<=n;i++){
+            while(!stack.isEmpty() && (i == n || arr[stack.peek()]>=(i<n? arr[i]:0))){
+                int height=arr[stack.pop()];
+                int width;
+                if (stack.isEmpty()){
+                    width=i;
+                }
+                else {
+                    width=i-stack.peek()-1;
+                }
+                maxArea=Math.max(maxArea,height*width);
+            }
+            stack.push(i);
         }
-        return maxi;
+        return maxArea;
     }
     public static void main(String[] args) {
         int[] arr = {2, 1, 5, 6, 2, 3, 1};
